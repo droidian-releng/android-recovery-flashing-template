8d53b10a9baa84f556c5670c428f06d646fb263c
r3vn <giuseppe.corti@protonmail.com>
2021-02-14 15:23:26 +0100

    [android-recovery-flashing-template] hybris-mobian changes

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
-# Ubport GSI installer Script
-# erfanoabdi @ xda-developers
+# hybris-mobian GSI installer Script
+# https://hybris-mobian.org
+
 OUTFD=/proc/self/fd/$1;
 VENDOR_DEVICE_PROP=`grep ro.product.vendor.device /vendor/build.prop | cut -d "=" -f 2 | awk '{print tolower($0)}'`;
 
@@ -7,24 +8,31 @@ VENDOR_DEVICE_PROP=`grep ro.product.vendor.device /vendor/build.prop | cut -d "=
 ui_print() { echo -e "ui_print $1\nui_print" > $OUTFD; }
 
 ## GSI install
-
-cp -fpr /data/ubports/data/* /data/;
+cp -fpr /data/hybris-mobian/data/* /data/;
 
 mkdir /s;
 mkdir /r;
 
-mount /data/system.img /s;
+# mount hybris-mobian rootfs
+mount /data/rootfs.img /r;
+
+# mount android gsi
+mount /r/var/lib/lxc/android/android-rootfs.img /s
+
+# Set udev rules
 ui_print "Setting udev rules";
 cat /s/ueventd*.rc /vendor/ueventd*.rc | grep ^/dev | sed -e 's/^\/dev\///' | awk '{printf "ACTION==\"add\", KERNEL==\"%s\", OWNER=\"%s\", GROUP=\"%s\", MODE=\"%s\"\n",$1,$3,$4,$2}' | sed -e 's/\r//' > /data/70-hybris-mobian.rules;
+
+# umount android gsi
 umount /s;
 
-mount /data/rootfs.img /r;
-mv /data/70-ubport.rules /r/etc/udev/rules.d/70-$VENDOR_DEVICE_PROP.rules;
+# move udev rules inside rootfs
+mv /data/70-hybris-mobian.rules /r/etc/udev/rules.d/70-$VENDOR_DEVICE_PROP.rules;
 
+# umount rootfs
 umount /r;
 
-mv /data/system.img /data/android-rootfs.img
-
+# resize rootfs
 ui_print "Resizing rootfs to 8GB";
 e2fsck -fy /data/rootfs.img
 resize2fs /data/rootfs.img 8G
